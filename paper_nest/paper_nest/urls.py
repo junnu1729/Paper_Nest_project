a86df770dfad5315204ef4a720cd9ff0f12c69ac
Juned Basha Gudupak <bashajuned143@gmail.com>
2025-04-15 22:45:14 +0530

    Update urls.py

--- paper_nest/paper_nest/urls.py
+++ paper_nest/paper_nest/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.static import serve
-from django.conf.urls import url
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
