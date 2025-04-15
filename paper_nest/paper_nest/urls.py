ac0168eb751cc87483f059ff5bc012fa182442cc
Juned Basha Gudupak <bashajuned143@gmail.com>
2025-04-15 22:53:16 +0530

    Update urls.py

--- paper_nest/paper_nest/urls.py
+++ paper_nest/paper_nest/urls.py
@@ -15,16 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.static import serve
-from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include("papernestapp.urls")),
-    path("auth/",include("authnest.urls")),
-    url(r'^media/(?p<path>.*)$',serve,{'document_root':        settings.MEDIA_ROOT}),
-    url(r'^static/(?p<path>.*)$',serve,{'document_root':settings.STATIC_ROOT}),
-]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    path("", include("papernestapp.urls")),
+    path("auth/", include("authnest.urls")),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
